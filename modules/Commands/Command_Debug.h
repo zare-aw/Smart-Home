d992f3513d08e293cd6c6c27a1e94cb3754e1076
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-02-25 17:43:29 -0800

    Command: Add Command debug macros.
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- modules/Commands/Command_Debug.h
+++ modules/Commands/Command_Debug.h
@@ -1,4 +1,17 @@
 #ifndef _Command_Debug_h
 #define _Command_Debug_h
 
+#define COMMAND_DEBUG
+
+#ifdef COMMAND_DEBUG
+#ifdef _StatusHandling_h
+#include "StatusHandling.h"
+#define FUNCTION_IN(Function) Function_In(Function)
+#define CMD_RETURN_SUCCESS(Function) RETURN_SUCCESS_FUNC(Function)
+#endif
+#else
+#define FUNCTION_IN(Function) 0
+#define CMD_RETURN_SUCCESS(Function) return 0;
+#endif
+
 #endif
