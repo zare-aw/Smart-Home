a569e7b221cda643e6cc795377a0532568c786ca
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-02-25 17:42:40 -0800

    Command: Add Cmd table struct and CMD_CREATE macro for creating commands.
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- modules/Commands/Command.h
+++ modules/Commands/Command.h
@@ -1,4 +1,27 @@
 #ifndef _Command_h
 #define _Command_h
 
+#include "Command_Defines.h"
+
+#define CFG_HELP
+
+typedef unsigned int Status_t;
+
+#pragma section=".cmd"
+
+typedef struct CmdTbl_s
+{
+  char *Name;         // Command name
+  uint8 MaxArgs;      // Maximum number of arguments
+  Status_t (*Cmd)(struct CmdTbl_s *, uint32, uint32, char *[]);  // Implementation function
+  char *Usage;        // Usage message
+#ifdef CFG_HELP
+  char *Help;         // Help message
+#endif
+} CmdTbl_t;
+
+#define CMD_CREATE(Name, MaxArgs, Cmd, Usage, Help) \
+__root __packed CmdTbl_t Cmd_##Name @ ".cmd" = {#Name, MaxArgs, Cmd, Usage, Help}
+
+
 #endif
