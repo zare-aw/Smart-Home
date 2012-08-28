5d8fc2521e3c3afd9b67db5f38ea0cec78515357
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-08-28 12:19:22 -0700

    Exception: Call Fatal_Abort instead of Abort in all exceptions hendlers
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- Modules/Debug/Exception.c
+++ Modules/Debug/Exception.c
@@ -8,7 +8,7 @@ __arm __noreturn void Undefined_Instructions_Handler(uint32 Address, uint32 Inst
   printc("\r>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
   printc("\r>>> Undefined Instruction Address = %X\n", Address);
   printc("\r>>> Undefined Instruction = %X\n", Instruction);
-  Abort(UNDEFINED_INSTRUCTIONS_EXCEPTION);
+  Fatal_Abort(-UNDEFINED_INSTRUCTIONS_EXCEPTION);
   while(1);
 }
 
@@ -19,7 +19,7 @@ __arm __noreturn void Software_Interrupt_Handler(uint32 Address, uint32 Instruct
   printc("\r>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
   printc("\r>>> Software Interrupt Address = %X\n", Address);
   printc("\r>>> Software Interrupt Instruction = %X\n", Instruction);
-  Abort(SOFTWARE_INTERRUPT_EXCEPTION);
+  Fatal_Abort(-SOFTWARE_INTERRUPT_EXCEPTION);
   while(1);
 }
 
@@ -29,7 +29,7 @@ __arm __noreturn void Prefetch_Abort_Handler(uint32 Address)
   printc("\r>>> Prefetch Abort ABORT !!!\n");
   printc("\r>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
   printc("\r>>> Prefetch Abort Address = %X\n", Address);
-  Abort(PREFETCH_ABORT_EXCEPTION);
+  Fatal_Abort(-PREFETCH_ABORT_EXCEPTION);
   while(1);
 }
 
@@ -40,7 +40,7 @@ __arm __noreturn void Data_Abort_Handler(uint32 Address, uint32 Instruction)
   printc("\r>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
   printc("\r>>> Data Abort Address = %X\n", Address);
   printc("\r>>> Data Abort Instruction = %X\n", Instruction);
-  Abort(DATA_ABORT_EXCEPTION);
+  Fatal_Abort(-DATA_ABORT_EXCEPTION);
   while(1);
 }
 
