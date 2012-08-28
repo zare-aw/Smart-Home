f0e9ea125b5e134f1e806323b9efb140dd19757a
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-08-27 17:50:37 -0700

    MAM: Switch to Func_Trace
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- SOC_HAL/MAM/MAM.c
+++ SOC_HAL/MAM/MAM.c
@@ -1,48 +1,53 @@
 #include "Global_Defines.h"
+#include "Func_Trace.h"
+#include "MAM_Func.h"
 #include "Includes.h"
 
 /*******************************************************************************
-    Funkcija za inicijalizacija na MAM. Kako vlezen argument se dava:
-    DISABLE za da go isklucis MAM 
-    ENABLE za da go vklucis MAM celosno
-    ako Osc = 12 MHz
-    Definirana za LPC2387
-*******************************************************************************/
+ * Funkcija za inicijalizacija na MAM. Kako vlezen argument se dava:
+ * DISABLE za da go isklucis MAM 
+ * ENABLE za da go vklucis MAM celosno
+ * ako Osc = 12 MHz
+ * Definirana za LPC2387
+ ******************************************************************************/
 Status_t MAM_Init(unsigned int a)
 {
-  Function_IN(MAM_INIT);
+  FuncIN(MAM_INIT);
+  
   unsigned int m;
+  
   switch (a)
   {
-  case ENABLE:
-    m = PLLSTAT_bit.MSEL + 1;
-    if(m < 2)                   // Nagodi go MAM Timing za CCLK < 20MHz
-    {
-      MAMCR = 0;
-      MAMTIM = 1;
-      MAMCR = 2;
-    }
-    if(m > 1)
-    {
-      if(m < 4)                 // Nagodi go MAM Timing za (20MHz < CCLK < 40MHz)
+    case ENABLE:
+      m = PLLSTAT_bit.MSEL + 1;
+      if(m < 2)                   // Nagodi go MAM Timing za CCLK < 20MHz
       {
         MAMCR = 0;
-        MAMTIM = 2;
+        MAMTIM = 1;
         MAMCR = 2;
       }
-      else                      // Nagodi go MAM Timing za CCLK > 40MHz
+      if(m > 1)
       {
-        MAMCR = 0;
-        MAMTIM = 3;
-        MAMCR = 2;
+        if(m < 4)                 // Nagodi go MAM Timing za (20MHz < CCLK < 40MHz)
+        {
+          MAMCR = 0;
+          MAMTIM = 2;
+          MAMCR = 2;
+        }
+        else                      // Nagodi go MAM Timing za CCLK > 40MHz
+        {
+          MAMCR = 0;
+          MAMTIM = 3;
+          MAMCR = 2;
+        }
       }
-    }
-    break;
-  case DISABLE:
+      break;
+    case DISABLE:
       MAMCR = 0;
-    break;
-  default:
-      CONTROL(0, INVALID_INIT_INPUT_PARAMETER);
+      break;
+    default:
+      Fatal_Abort(-INVALID_INIT_INPUT_PARAMETER);
   }
-  RETURN_SUCCESS();
+  
+  EXIT_SUCCESS_FUNC(MAM_INIT);
 }
---
+++ SOC_HAL/MAM/MAM_Func.h
@@ -0,0 +1,6 @@
+#ifndef _MAM_Func_h
+#define _MAM_Func_h
+
+#define MAM_INIT      MAM_OFFSET | 0x01
+
+#endif
