9ca20c917f6be277c85e69dbb5741fbf52bf1056
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-12-10 22:51:48 +0100

    Menu: Time implementation: Add more states
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- Modules/Menu/Menu_Impl/Time/Menu_Time.c
+++ Modules/Menu/Menu_Impl/Time/Menu_Time.c
@@ -3,7 +3,19 @@
 #include "StatusHandling.h"
 #include "Menu.h"
 #include "Menu_Func.h"
+#include "SurfaceFlinger.h"
+#include "Console.h"
 
+Status_t Menu_Set_Time(struct Menu_State_s *Menu_State_p, const uint32 Key, void *Temp_p)
+{
+  clrd();
+  
+  printd(1, "Test");
+  
+  syncd();
+  
+  return SUCCESS;
+}
 
 uint8 TimePath[] = {0};
 MENU_STATE_CREATE(
@@ -15,4 +27,41 @@ MENU_STATE_CREATE(
   CANCEL_KEY_EVENT,
   NULL,
   "Time"
+);
+
+uint8 TimeAlarmPath[] = {0, 0};
+MENU_STATE_CREATE(
+  TimeAlarm,
+  TimeAlarmPath,
+  2,
+  0x00,
+  ENTER_KEY_EVENT | \
+  CANCEL_KEY_EVENT,
+  NULL,
+  "Time Alarm"
+);
+
+uint8 SetTimePath[] = {0, 1};
+MENU_STATE_CREATE(
+  SetTime,
+  SetTimePath,
+  2,
+  0x00,
+  ENTER_KEY_EVENT | \
+  CANCEL_KEY_EVENT,
+  Menu_Set_Time,
+  "Set Time"
+);
+
+uint8 SettingTimePath[] = {0, 1, 0};
+MENU_STATE_CREATE(
+  SettingTime,
+  SettingTimePath,
+  3,
+  MENU_LAST_STATE | \
+  MENU_NO_DISPLAY_UPDATE,
+  ENTER_KEY_EVENT | \
+  CANCEL_KEY_EVENT,
+  Menu_Set_Time,
+  ""
 );
