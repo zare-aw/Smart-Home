3f4d60d16da0c5d3ca0eb6050dea1a91cdfb6f0b
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-12-14 23:37:34 +0100

    Cmd X: Add numbers, menu and exit console mapped keys
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- Modules/Commands/cmd_src/cmd_x.c
+++ Modules/Commands/cmd_src/cmd_x.c
@@ -14,16 +14,16 @@ Status_t Do_X(Cmd_Tbl_t *Cmd_Tbl, uint32 flag, uint32 argc, char *argv[])
   {
     switch(*argv[0])
     {
-      case '2':   // Down
+      case 's':   // Down
         Down_Key_Callback(NULL);
         break;
-      case '8':   // Up
+      case 'w':   // Up
         Up_Key_Callback(NULL);
         break;
-      case '4':   // Left
+      case 'a':   // Left
         Left_Key_Callback(NULL);
         break;
-      case '6':   // Right
+      case 'd':   // Right
         Right_Key_Callback(NULL);
         break;
       case '-':   // Cancel
@@ -31,6 +31,42 @@ Status_t Do_X(Cmd_Tbl_t *Cmd_Tbl, uint32 flag, uint32 argc, char *argv[])
         break;
       case '+':   // Enter
         Enter_Key_Callback(NULL);
+        break;
+      case '*':   // Menu
+        Menu_Key_Callback(NULL);
+        break;
+      case '/':   // Exit
+        Exit_Key_Callback(NULL);
+        break;
+      case '1':   // Number 1
+        Num1_Key_Callback(NULL);
+        break;
+      case '2':   // Number 2
+        Num2_Key_Callback(NULL);
+        break;
+      case '3':   // Number 3
+        Num3_Key_Callback(NULL);
+        break;
+      case '4':   // Number 4
+        Num4_Key_Callback(NULL);
+        break;
+      case '5':   // Number 5
+        Num5_Key_Callback(NULL);
+        break;
+      case '6':   // Number 6
+        Num6_Key_Callback(NULL);
+        break;
+      case '7':   // Number 7
+        Num7_Key_Callback(NULL);
+        break;
+      case '8':   // Number 8
+        Num8_Key_Callback(NULL);
+        break;
+      case '9':   // Number 9
+        Num9_Key_Callback(NULL);
+        break;
+      case '0':   // Number 0
+        Num0_Key_Callback(NULL);
         break;
       default:
         EXIT_FUNC(CMD_INVALID_ARGUMENT, DO_X);
