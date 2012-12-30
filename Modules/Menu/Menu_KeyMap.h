81c6ad4ee0b201c4262c2ad1c725d7a20545bfa4
Zarko Kostadinovski <zare_aw@yahoo.com>
2012-12-30 17:03:19 +0100

    Menu: Update Key Map
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- Modules/Menu/Menu_KeyMap.h
+++ Modules/Menu/Menu_KeyMap.h
@@ -2,61 +2,61 @@
 #define _Menu_KeyMap_h
 
 
-#define POWER_KEY_ADDRESS       6
-#define POWER_KEY_COMMAND       53
+#define POWER_KEY_ADDRESS       8
+#define POWER_KEY_COMMAND       12
 
-#define UP_KEY_ADDRESS          5
+#define UP_KEY_ADDRESS          8
 #define UP_KEY_COMMAND          16
 
-#define DOWN_KEY_ADDRESS        5
+#define DOWN_KEY_ADDRESS        8
 #define DOWN_KEY_COMMAND        17
 
-#define LEFT_KEY_ADDRESS        5
+#define LEFT_KEY_ADDRESS        8
 #define LEFT_KEY_COMMAND        21
 
-#define RIGHT_KEY_ADDRESS       5
+#define RIGHT_KEY_ADDRESS       8
 #define RIGHT_KEY_COMMAND       22
 
-#define ENTER_KEY_ADDRESS       5
-#define ENTER_KEY_COMMAND       23
+#define ENTER_KEY_ADDRESS       8
+#define ENTER_KEY_COMMAND       10
 
-#define CANCEL_KEY_ADDRESS      5
-#define CANCEL_KEY_COMMAND      19
+#define CANCEL_KEY_ADDRESS      8
+#define CANCEL_KEY_COMMAND      34
 
-#define MENU_KEY_ADDRESS        5
+#define MENU_KEY_ADDRESS        8
 #define MENU_KEY_COMMAND        18
 
-#define EXIT_KEY_ADDRESS        5
-#define EXIT_KEY_COMMAND        26
+#define EXIT_KEY_ADDRESS        8
+#define EXIT_KEY_COMMAND        19
 
-#define NUM_1_KEY_ADDRESS       5
+#define NUM_1_KEY_ADDRESS       8
 #define NUM_1_KEY_COMMAND       1
 
-#define NUM_2_KEY_ADDRESS       5
+#define NUM_2_KEY_ADDRESS       8
 #define NUM_2_KEY_COMMAND       2
 
-#define NUM_3_KEY_ADDRESS       5
+#define NUM_3_KEY_ADDRESS       8
 #define NUM_3_KEY_COMMAND       3
 
-#define NUM_4_KEY_ADDRESS       5
+#define NUM_4_KEY_ADDRESS       8
 #define NUM_4_KEY_COMMAND       4
 
-#define NUM_5_KEY_ADDRESS       5
+#define NUM_5_KEY_ADDRESS       8
 #define NUM_5_KEY_COMMAND       5
 
-#define NUM_6_KEY_ADDRESS       5
+#define NUM_6_KEY_ADDRESS       8
 #define NUM_6_KEY_COMMAND       6
 
-#define NUM_7_KEY_ADDRESS       5
+#define NUM_7_KEY_ADDRESS       8
 #define NUM_7_KEY_COMMAND       7
 
-#define NUM_8_KEY_ADDRESS       5
+#define NUM_8_KEY_ADDRESS       8
 #define NUM_8_KEY_COMMAND       8
 
-#define NUM_9_KEY_ADDRESS       5
+#define NUM_9_KEY_ADDRESS       8
 #define NUM_9_KEY_COMMAND       9
 
-#define NUM_0_KEY_ADDRESS       5
+#define NUM_0_KEY_ADDRESS       8
 #define NUM_0_KEY_COMMAND       0
 
 #define VOL_UP_KEY_ADDRESS      0
@@ -65,13 +65,13 @@
 #define VOL_DOWN_KEY_ADDRESS    0
 #define VOL_DOWN_KEY_COMMAND    0
 
-#define CH_UP_KEY_ADDRESS       5
+#define CH_UP_KEY_ADDRESS       8
 #define CH_UP_KEY_COMMAND       32
 
-#define CH_DOWN_KEY_ADDRESS     5
+#define CH_DOWN_KEY_ADDRESS     8
 #define CH_DOWN_KEY_COMMAND     33
 
 #define MUTE_KEY_ADDRESS        0
-#define MUTE_KEY_COMMAND        0
+#define MUTE_KEY_COMMAND        13
 
 #endif  // _Menu_KeyMap_h
