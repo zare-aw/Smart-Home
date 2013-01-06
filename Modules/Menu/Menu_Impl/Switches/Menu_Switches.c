8bf701de8f02b6b91d6e16f11cfb4a3eb1ef524f
Zarko Kostadinovski <zare_aw@yahoo.com>
2013-01-06 18:47:15 +0100

    Menu: Move Switches State in Menu_Switches_State.c file
    Signed-off-by: Zarko Kostadinovski <zare_aw@yahoo.com>

--- Modules/Menu/Menu_Impl/Switches/Menu_Switches.c
+++ Modules/Menu/Menu_Impl/Switches/Menu_Switches.c
@@ -5,11 +5,10 @@
 #include "StatusHandling.h"
 #include "Menu.h"
 #include "Menu_Func.h"
+#include "Menu_Switches.h"
 #include "Out.h"
 #include "SurfaceFlinger.h"
 
-#define NO_OF_SWITCHES                NO_OF_OUTS
-#define SWITCHES_STRING_LENGTH        8
 #define SWITCHES_SET_STRING_LENGTH    X_SIZE + 1
 
 #define SW_1_SET_STR    0x00000001
@@ -28,41 +27,6 @@ char SW_Name[NO_OF_SWITCHES][SWITCHES_STRING_LENGTH] = {"SW_1", "SW_2",
 char SW_Set_Name[NO_OF_SWITCHES][SWITCHES_SET_STRING_LENGTH] = {0};
 
 static Status_t Menu_Switch_Setting(struct Menu_State_s *Menu_State_p, const uint32 Key, void *Ptr);
-
-/*******************************************************************************
- *
- ******************************************************************************/
-static Status_t Switches_State_Display(struct Menu_State_s *Menu_State_p, const uint32 Key, void *Ptr)
-{
-  FuncIN(SWITCHES_STATE_DISPLAY);
-  
-  Out_t Out_s;
-  
-  switch(Key)
-  {
-    case ENTER_KEY_EVENT:
-      Out_Get_State(OUT_PINS, &Out_s);
-      
-      clrd();
-      printd(1, "%s:%d%*c%s:%d", SW_Name[0], Out_s.S_1, (8 - strlen(SW_Name[0])), ' ', SW_Name[1], Out_s.S_2);
-      printd(2, "%s:%d%*c%s:%d", SW_Name[2], Out_s.S_3, (8 - strlen(SW_Name[2])), ' ', SW_Name[3], Out_s.S_4);
-      printd(3, "%s:%d%*c%s:%d", SW_Name[4], Out_s.S_5, (8 - strlen(SW_Name[4])), ' ', SW_Name[5], Out_s.S_6);
-      syncd();
-      break;
-    case CANCEL_KEY_EVENT:
-      clrd();
-      break;
-    case UP_KEY_EVENT:
-      break;
-    case DOWN_KEY_EVENT:
-      break;
-    default:
-      Fatal_Abort(-INVALID_INPUT_PARAMETER);
-  } // switch(Key)
-  
-  EXIT_SUCCESS_FUNC(SWITCHES_STATE_DISPLAY);
-}
-FUNC_REGISTER(SWITCHES_STATE_DISPLAY, Switches_State_Display);
 
 /*******************************************************************************
  *
@@ -525,25 +489,6 @@ MENU_STATE_CREATE(
   "Switches"
 );
 
-uint8 SwitchesStatePath[] = {2, 0};
-MENU_STATE_CREATE(
-  // Name
-  SwitchesState,
-  // Path
-  SwitchesStatePath,
-  // Max Level
-  2,
-  // Flags
-  0x00,
-  // Possible Keys
-  ENTER_KEY_EVENT | \
-  CANCEL_KEY_EVENT,
-  // Callback
-  Switches_State_Display,
-  // String
-  "Switches State"
-);
-
 uint8 SwitchesSetPath[] = {2, 1};
 MENU_STATE_CREATE(
   // Name
@@ -561,28 +506,6 @@ MENU_STATE_CREATE(
   Menu_Switches_Set,
   // String
   "Switches Set"
-);
-
-uint8 SwitchesStateDisplayPath[] = {2, 0, 0};
-MENU_STATE_CREATE(
-  // Name
-  SwitchesStateDisplay,
-  // Path
-  SwitchesStateDisplayPath,
-  // Max Level
-  3,
-  // Flags
-  MENU_LAST_STATE | \
-  MENU_NO_DISPLAY_UPDATE,
-  // Possible Keys
-  ENTER_KEY_EVENT | \
-  CANCEL_KEY_EVENT | \
-  UP_KEY_EVENT | \
-  DOWN_KEY_EVENT,
-  // Callback
-  Switches_State_Display,
-  // String
-  ""
 );
 
 uint8 Switch_1_SetPath[] = {2, 1, 0};
---
+++ Modules/Menu/Menu_Impl/Switches/Menu_Switches.h
@@ -0,0 +1,11 @@
+#ifndef _Menu_Switches_h
+#define _Menu_Switches_h
+
+#include "Out.h"
+
+#define NO_OF_SWITCHES                NO_OF_OUTS
+#define SWITCHES_STRING_LENGTH        8
+
+extern char SW_Name[NO_OF_SWITCHES][SWITCHES_STRING_LENGTH];
+
+#endif // _Menu_Switches_h
